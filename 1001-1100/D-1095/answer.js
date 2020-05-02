7fc56a3c966bfbc4f3b8e27d1289e6ffa143b1a1
carl <oowoolf@gmail.com>
2020-05-02 22:11:17 +0800

    2020-05-02 山脉问题

--- 1001-1100/D-1095/answer.js
+++ 1001-1100/D-1095/answer.js
@@ -21,5 +21,38 @@
  * @return {number}
  */
 var findInMountainArray = function(target, mountainArr) {
-    
+    let binSearch = function(l, r) {
+        if (l > r) {
+            return -1;
+        }
+        let mid = Math.trunc((l+r)/2);
+        let val = mountainArr.get(mid);
+        if (val == target) {
+            return mid;
+        } else if ()
+        if (val > target) {
+            return binSearch(l, mid-1);
+        } else {
+            return binSearch(mid+1, r);
+        }
+    }
+    let findPeak = function(l, r) {
+        if (l == r) {
+            return l;
+        }
+        let mid =  Math.trunc((l+r)/2);
+        if (mountainArr.get(mid) > mountainArr.get(mid+1)) {
+            return findPeak(l, mid);
+        } else {
+            return findPeak(mid+1, r);
+        }
+    }
+    let len = mountainArr.length();
+    let peak = findPeak(0, len-1);
+    let left = binSearch(0, peak);
+    if (-1 !== left) {
+        return left;
+    } else {
+        return binSearch(peak, len-1);
+    }
 };
