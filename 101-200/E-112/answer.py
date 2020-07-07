c92dcb87840a785084dc4ff979c04dc02fe8b4a7
Dapianzi Carl <oowoolf@gmail.com>
2020-07-07 15:09:00 +0800

    每日一题

---
+++ 101-200/E-112/answer.py
@@ -0,0 +1,17 @@
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution:
+    def hasPathSum(self, root: TreeNode, sum: int) -> bool:
+        if root is None:
+            return False
+        elif root.left is None && root.right is None:
+            return root.val == sum
+        else:
+            return self.hasPathSum(root.left, sum-root.val) or self.hasPathSum(root.right, sum-root.val)
+
+
